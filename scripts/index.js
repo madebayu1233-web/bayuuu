a913fb49ee2cfa7f83f38dcd812798790f31ae4b
madebayu1233-web <madebayu1233@gmail.com>
2026-09-21 22:48:04 +0800

    Update login credentials in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,11 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "user" && password === "user") {
+  if (username === "dellacantik" && password === "051025") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Selamat datang sayanggg",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
